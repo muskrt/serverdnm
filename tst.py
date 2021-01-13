944de232f051da07b3795a5ab430222096e0cc7f
muskrt <kurtmustafa5003@gmail.com>
2021-01-13 14:49:54 +0300

    os function

--- tst.py
+++ tst.py
@@ -2,9 +2,9 @@ import os
 import sys
 def clear():
     print('clear')
-<<<<<<< HEAD
+
 def newsystem():
     print('welcome again sir')
-=======
-    print('tst')
->>>>>>> bd7a96c2ffcf114043965349f8743460cf542100
+def integrate():
+    os.system('cls')
+
